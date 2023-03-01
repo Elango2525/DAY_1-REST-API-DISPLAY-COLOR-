6553b102b6450cb8464d565521c8ec601d6de2e5
Elango2525 <727721euit035@skcet.ac.in>
2023-03-01 21:51:19 +0530

    first commit

---
+++ src/main/java/com/example/Student/DisplaycolorApplication.java
@@ -0,0 +1,13 @@
+package com.example.Student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisplaycolorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisplaycolorApplication.class, args);
+	}
+
+}
